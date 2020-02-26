9c6ef12f05966c529b4462f43c70c902a12fa5f5
Ashkan Abbasi <airashkan@yahoo.com>
2020-02-26 10:14:29 -0700

    Test Item exists and initialize Item

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,8 @@
 class Item
-end   
+  attr_reader :name, :price
+
+  def initialize(item_params)
+    @name = item_params[:name]
+    @price = item_params[:price]
+  end
+end
--- test/item_test.rb
+++ test/item_test.rb
@@ -9,14 +9,18 @@ class ItemTest < Minitest::Test
 
   def setup
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists
+    assert_instance_of Item, @item1
+    assert_instance_of Item, @item2
   end
 
 
 end
 
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
+
 # pry(main)> item2 = Item.new({name: 'Tomato', price: '$0.50'})
 # #=> #<Item:0x007f9c565c0ce8...>
 #
