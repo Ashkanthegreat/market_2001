8a33842d4fdc94541faf087608d937a785978f71
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-14 19:44:12 -0600

    Add potential revenue test and method

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -9,7 +9,12 @@ class VendorTest < Minitest::Test
 
   def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exists
@@ -37,6 +42,18 @@ class VendorTest < Minitest::Test
     @vendor.stock(@item2, 12)
     expected = {@item1 => 55, @item2 => 12}
     assert_equal expected, @vendor.inventory
+  end
+
+  def test_can_calculate_potential_revenue
+    @vendor.stock(@item1, 35)
+    @vendor.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    assert_equal 29.75, @vendor.potential_revenue
+    assert_equal 345.00, @vendor2.potential_revenue
+    assert_equal 48.75, @vendor3.potential_revenue
   end
 
 
